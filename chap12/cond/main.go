39e120c0614c0905a03b8b7bf84ef4573fc43a7a
Kevin Yuan <farproc@gmail.com>
2024-07-29 05:45:58 +0800

    Simplify cond example.

--- chap12/cond/main.go
+++ chap12/cond/main.go
@@ -10,46 +10,25 @@ func main() {
 	var event string
 	var lock sync.RWMutex
 	var cond = sync.Cond{L: lock.RLocker()} // 接收者只读
-	var group sync.WaitGroup
 
 	var receiveEvent = func(n int) {
-		defer group.Done()
-		for {
-			cond.L.Lock()
+		cond.L.Lock()
+		defer cond.L.Unlock()
+		for event == "" {
 			cond.Wait()
-			fmt.Printf("goroutine %v received %#v\n", n, event)
-			if event == "exit" {
-				cond.L.Unlock()
-				return
-			}
-			cond.L.Unlock()
 		}
+		fmt.Printf("goroutine %v received %#v\n", n, event)
 	}
 
-	group.Add(2)
 	go receiveEvent(1)
 	go receiveEvent(2)
 
 	time.Sleep(time.Second)
 
 	lock.Lock()
-	event = "broadcast event"
+	event = "event 1"
 	lock.Unlock()
 	cond.Broadcast() // 唤醒所有 Wait
 
-	time.Sleep(time.Second)
-
-	lock.Lock()
-	event = "signal event"
-	lock.Unlock()
-	cond.Signal() // 唤醒一个 Wait
-
-	time.Sleep(time.Second)
-
-	lock.Lock()
-	event = "exit"
-	lock.Unlock()
-	cond.Broadcast()
-
-	group.Wait()
+	time.Sleep(time.Second * 2)
 }
