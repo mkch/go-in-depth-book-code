f201efae26e24310b52ee64ab5171c33393da39a
Kevin Yuan <farproc@gmail.com>
2024-08-05 14:19:23 +0800

    Add chap14.

---
+++ chap14/basic/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"maps"
+	"net/http"
+	"net/url"
+	"slices"
+)
+
+// Int 为有符号整数类型.
+type Int interface {
+	~int8 | ~int | ~int16 | ~int32 | ~int64
+}
+
+// Sum 计算参数之和
+func Sum[T Int](values ...T) (r T) {
+	for _, v := range values {
+		r += v // !!语法错误!!
+	}
+	return
+}
+
+type Node[T any] struct {
+	Value T
+	Next  *Node[T]
+}
+
+// List 是一个链表
+type List[T any] struct {
+	Head *Node[T]
+}
+
+// Size 返回 l 的长度
+func (l *List[T]) Size() (r int) {
+	for p := l.Head; p != nil; p = p.Next {
+		r++
+	}
+	return
+}
+
+type IntStringer interface {
+	String() string
+	Int
+}
+
+func SumStr[T IntStringer](values ...T) string {
+	var sum T
+	for _, v := range values {
+		sum += v
+	}
+	return sum.String()
+}
+
+// Scale 返回 s 的一个拷贝, 其中每个元素都乘以 c
+func Scale[S ~[]E, E Int](s S, c E) S {
+	r := make(S, len(s))
+	for i, v := range s {
+		r[i] = v * c
+	}
+	return r
+}
+
+// Point 为一个多维的点
+type Point []int32
+
+func (p Point) String() string {
+	return fmt.Sprintf("Point %v", ([]int32)(p))
+}
+
+func main() {
+	var v = []int32{1, 2, 3}
+	v2 := Scale(v, 2)
+	fmt.Println(v2)
+
+	var pt = Point{1, 2, 3}
+	pt2 := Scale(pt, 2)
+	fmt.Println(pt2.String())
+}
+
+func DiscardG[T io.Reader](r T) error {
+	_, err := io.Copy(io.Discard, r)
+	return err
+}
+
+func Discard(r io.Reader) error {
+	_, err := io.Copy(io.Discard, r)
+	return err
+}
+
+func F[T comparable](a T) {
+	_ = a == a
+	// any 没有实现 comparable
+	//a = any(0)                         // !!语法错误!!
+	//a = any(struct{ F map[int]int }{}) // !!语法错误!!
+}
+
+func UseF() {
+	// any 可满足 comparable
+	F(any(0))
+	F(any(struct{ F map[int]int }{}))
+}
+
+func UseSlicesSort() {
+	var intSlice = []int{3, 4, 1}
+	slices.Sort[[]int, int](intSlice)
+	slices.Sort(intSlice)
+}
+
+func Copy(dest http.Header, src url.Values) {
+	maps.Copy(dest, src)
+}
---
+++ chap14/practice/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+)
+
+// DecodeBody 把 r 的内容解码后放入 dest 所指向的变量中
+// 类似 json.Unmarshal()
+func DecodeBody(r io.Reader, dest any) error {
+	// 实现代码省略
+	return nil
+}
+
+// DecodeQuey 把 m 解码后放入 dest 所指向的变量中
+// 类似 json.Unmarshal()
+func DecodeQuey(m url.Values, dest any) error {
+	// 实现代码省略
+	return nil
+}
+
+// DecodeQuey 把 m 解码后放入 dest 所指向的变量中
+// 类似 json.Unmarshal()
+func DecodeHeader(m http.Header, dest any) error {
+	// 实现代码省略
+	return nil
+}
+
+// Validate 对 v 进行校验
+// 如果 v 是 struct, 校验将根据 v 的 field tag 进行
+// https://github.com/go-playground/validator 就是一个类似的校验库
+func Validate(v any) error {
+	return nil
+}
+
+func Decode[T any](w http.ResponseWriter, decode func(T, any) error, arg T, dest any) (ok bool) {
+	var err error
+	if err = decode(arg, dest); err == nil {
+		err = Validate(dest)
+	}
+	if ok = err == nil; !ok {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	return
+}
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	type Body struct{}
+	type Query struct{}
+	type Header struct{}
+
+	var body Body
+	if err := DecodeBody(r.Body, &body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	} else if err = Validate(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var query Query
+	if err := DecodeQuey(r.URL.Query(), &query); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	} else if err = Validate(&query); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	var header Header
+	if err := DecodeHeader(r.Header, &body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	} else if err = Validate(&header); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// ...
+}
+
+func Handle2(w http.ResponseWriter, r *http.Request) {
+	type Body struct{}
+	type Query struct{}
+	type Header struct{}
+
+	var body Body
+	var query Query
+	var header Header
+	if !Decode[io.Reader](w, DecodeBody, r.Body, &body) ||
+		!Decode(w, DecodeQuey, r.URL.Query(), &query) ||
+		!Decode(w, DecodeHeader, r.Header, &header) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// ...
+}
