99014aeaf0f8f85d2392f275e31b634dc6927fa5
Kevin Yuan <farproc@gmail.com>
2024-10-02 01:06:04 +0800

    Update main.go

--- chap6/sched/main.go
+++ chap6/sched/main.go
@@ -6,13 +6,12 @@ import (
 	"time"
 )
 
-// Instruction 为 VM 中的一条指令
+// Instruction 为 VM 中的一条指令.
 //
 // 此 VM 为一个无栈 VM, 其中函数无参数也无返回值.
 // 一个函数被编译为若干条指令加一个 nil.
-type Instruction struct {
-	F func(*Thread)
-}
+// 因为没有栈, 所以函数调用只能出现在最顶层 [NewG] 中.
+type Instruction func(*Thread)
 
 // Thread 为 VM 中的一个本地线程.
 type Thread struct {
@@ -23,19 +22,19 @@ type Thread struct {
 
 // NewThread 启动一个 VM 本地线程.
 func NewThread(id int) {
-	m := &Thread{id: id}
+	t := &Thread{id: id}
 	// 在此 VM 中, 本地线程即 goroutine
 	go func() {
 		for {
-			m.g = DequeueG() // 从 gq 中取出一个待执行的 G
-			m.pc = m.g.pc    // 使用该 G 的 pc 继续执行
-			for inst := Code[m.pc]; inst != nil; inst = Code[m.pc] {
-				m.pc++ // 指向下一条指令
-				if inst.F != nil {
-					inst.F(m) // 执行当前指令
+			t.g = DequeueG() // 从 gq 中取出一个待执行的 G
+			t.pc = t.g.pc    // 使用该 G 的 pc 继续执行
+			for inst := Code[t.pc]; inst != nil; inst = Code[t.pc] {
+				t.pc++ // 指向下一条指令
+				if inst != nil {
+					inst(t) // 执行当前指令
 				}
 			}
-			m.g = nil // 当前 G 执行完毕, 此 Thread 空闲
+			t.g = nil // 当前 G 执行完毕, 此 Thread 空闲
 		}
 	}()
 	return
@@ -93,32 +92,32 @@ func Schedule(m *Thread) {
 }
 
 // Code 为 VM 的代码段.
-var Code []*Instruction
+var Code []Instruction
 
 func main() {
-	Code = []*Instruction{
+	Code = []Instruction{
 		// 第一个函数 func1
-		/*0*/ {func(m *Thread) { fmt.Printf("Thread#%d func1 A\n", m.id) }},
-		/*1*/ {Schedule}, // 编译器自动添加的调度指令
-		/*2*/ {func(m *Thread) { fmt.Printf("Thread#%d func1 B\n", m.id) }},
-		/*3*/ {func(m *Thread) { fmt.Printf("Thread#%d func1 C\n", m.id) }},
+		/*0*/ func(t *Thread) { fmt.Printf("Thread#%d func1 A\n", t.id) },
+		/*1*/ Schedule, // 编译器自动添加的调度指令
+		/*2*/ func(t *Thread) { fmt.Printf("Thread#%d func1 B\n", t.id) },
+		/*3*/ func(t *Thread) { fmt.Printf("Thread#%d func1 C\n", t.id) },
 		/*4*/ nil,
 		// 第二个函数 func2
-		/*5*/ {func(m *Thread) { fmt.Printf("Thread#%d func2 D\n", m.id) }},
-		/*6*/ {func(m *Thread) { fmt.Printf("Thread#%d func2 E\n", m.id) }},
-		/*7*/ {Schedule}, // 编译器自动添加的调度指令
-		/*8*/ {func(m *Thread) { fmt.Printf("Thread#%d func2 F\n", m.id) }},
-		/*9*/ {func(m *Thread) { fmt.Printf("Thread#%d func2 G\n", m.id) }},
+		/*5*/ func(t *Thread) { fmt.Printf("Thread#%d func2 D\n", t.id) },
+		/*6*/ func(t *Thread) { fmt.Printf("Thread#%d func2 E\n", t.id) },
+		/*7*/ Schedule, // 编译器自动添加的调度指令
+		/*8*/ func(t *Thread) { fmt.Printf("Thread#%d func2 F\n", t.id) },
+		/*9*/ func(t *Thread) { fmt.Printf("Thread#%d func2 G\n", t.id) },
 		/*10*/ nil,
 	}
 
 	NewThread(1)
 	NewThread(2)
 
-	// NewG(0) 代表函数 func1 (地址为 0) 启动一个 G
+	// NewG(0) 代表使用函数 func1 (地址为 0) 启动一个 G
 	// 此函数包含 4 条指令 (包括结尾的nil)
 	NewG(0)
-	// NewG(0) 代表使用函数 func2 (地址为 5), 启动一个 G
+	// NewG(5) 代表使用函数 func2 (地址为 5), 启动一个 G
 	// 此函数包含 6 条指令 (包括结尾的nil)
 	NewG(5)
 
