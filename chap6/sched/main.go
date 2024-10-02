d080d42c2d275d8b7139999b2bce7260ee3ef9d2
Kevin Yuan <farproc@gmail.com>
2024-10-02 20:56:38 +0800

    Update main.go

--- chap6/sched/main.go
+++ chap6/sched/main.go
@@ -29,12 +29,11 @@ func NewThread(id int) {
 			t.g = DequeueG() // 从 gq 中取出一个待执行的 G
 			t.pc = t.g.pc    // 使用该 G 的 pc 继续执行
 			for inst := Code[t.pc]; inst != nil; inst = Code[t.pc] {
-				t.pc++ // 指向下一条指令
-				if inst != nil {
-					inst(t) // 执行当前指令
-				}
+				t.pc++  // 指向下一条指令
+				inst(t) // 执行当前指令
 			}
-			t.g = nil // 当前 G 执行完毕, 此 Thread 空闲
+			// 当前 G 执行完毕, 此 Thread 空闲, 释放对 G 的引用
+			t.g = nil
 		}
 	}()
 	return
@@ -45,6 +44,12 @@ type G struct {
 	pc int // 指向下一条要指向的指令
 }
 
+// gq 为等待执行的 G 队列.
+var gq []*G
+
+// gqCond 为 gq 用到的条件变量.
+var gqCond = sync.Cond{L: &sync.Mutex{}}
+
 // NewG 创建一个并发单元.
 func NewG(pc int) {
 	g := &G{pc: pc}
@@ -53,12 +58,6 @@ func NewG(pc int) {
 	gq = append(gq, g)
 	gqCond.Signal()
 }
-
-// gq 为等待执行的 G 队列.
-var gq []*G
-
-// gqCond 为 gq 用到的条件变量.
-var gqCond = sync.Cond{L: &sync.Mutex{}}
 
 // DequeueG 从 gq 队列中取出一个 G,
 // 如果当前队列为空, 则阻塞等待直到有 G 加入 gq 为止.
@@ -74,19 +73,19 @@ func DequeueG() (g *G) {
 }
 
 // Schedule 为调度指令, 此指令执行一次 G 调度.
-// 本函数返回后 m 所关联的 G 会让出执行时间,
-// 使得 gq 中的 G 有机会执行.
-func Schedule(m *Thread) {
+// 本函数返回后, m 所关联的 G 会让出执行时间, 使得 gq 中的 G 有机会执行.
+func Schedule(t *Thread) {
 	gqCond.L.Lock()
 	defer gqCond.L.Unlock()
 	if len(gq) > 0 {
-		out := m.g    // out 为被调度出去的 G
-		out.pc = m.pc // 记录 out 的 pc 寄存器
+		out := t.g    // out 为被调度出去的 G
+		out.pc = t.pc // 记录 out 的 pc 寄存器
 		// 准备运行新 G
-		m.g = gq[0]
-		m.pc = m.g.pc // 把新 G 的 pc 作为当前 pc 寄存器
+		t.g = gq[0]
+		t.pc = t.g.pc // 把新 G 的 pc 作为当前 pc 寄存器
 		// 把 out 加入等待队列
 		gq = append(gq[1:], out)
+		gqCond.Signal()
 	}
 	return
 }
@@ -105,21 +104,29 @@ func main() {
 		// 第二个函数 func2
 		/*5*/ func(t *Thread) { fmt.Printf("Thread#%d func2 D\n", t.id) },
 		/*6*/ func(t *Thread) { fmt.Printf("Thread#%d func2 E\n", t.id) },
-		/*7*/ Schedule, // 编译器自动添加的调度指令
-		/*8*/ func(t *Thread) { fmt.Printf("Thread#%d func2 F\n", t.id) },
+		/*7*/ func(t *Thread) { fmt.Printf("Thread#%d func2 F\n", t.id) },
+		/*8*/ Schedule, // 编译器自动添加的调度指令
 		/*9*/ func(t *Thread) { fmt.Printf("Thread#%d func2 G\n", t.id) },
 		/*10*/ nil,
+		// 第三个函数 func3
+		/*11*/ func(t *Thread) { fmt.Printf("Thread#%d func3 H\n", t.id) },
+		/*12*/ Schedule, // 编译器自动添加的调度指令
+		/*13*/ func(t *Thread) { fmt.Printf("Thread#%d func3 I\n", t.id) },
+		/*14*/ nil,
 	}
 
 	NewThread(1)
 	NewThread(2)
 
 	// NewG(0) 代表使用函数 func1 (地址为 0) 启动一个 G
-	// 此函数包含 4 条指令 (包括结尾的nil)
+	// 此函数包含 5 条指令 (包括结尾的nil)
 	NewG(0)
 	// NewG(5) 代表使用函数 func2 (地址为 5), 启动一个 G
 	// 此函数包含 6 条指令 (包括结尾的nil)
 	NewG(5)
+	// NewG(11) 代表使用函数 func3 (地址为 11), 启动一个 G
+	// 此函数包含 3 条指令 (包括结尾的nil)
+	NewG(11)
 
 	time.Sleep(time.Second * 1)
 }
